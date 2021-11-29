35d0973ebd1f53f05f0eee1a1e155ad08b70e133
Filip, Vlad <vlad.filip@dlr.de>
2021-11-29 15:17:22 +0100

    Added old key bindings. Minor improvements.

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -27,27 +27,28 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "main", "web", "ide", "code", "visual", "text", "media" };
 
-static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-};
-
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "[T]",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+};
+
+static const Rule rules[] = {
+	/* class            instance    title       tags mask     isfloating   monitor */
+	{ "Firefox",        NULL,       NULL,       1 << 1,       False,       -1 },
+	{ "jetbrains-idea", NULL,       NULL,       1 << 2,       False,       -1 },
+	{ "Gedit",          NULL,       NULL,       1 << 3,       False,       -1 },
+	{ "MPlayer",        NULL,       NULL,       1 << 7,       False,       -1 },
+	{ "evince",	        NULL,	    NULL,       1 << 5,       False,       -1 },
+	{ "libreoffice",    NULL,       NULL,       1 << 5,       False,       -1 },
+    { "inkscape",       NULL,       NULL,       1 << 6,       False,       -1 },
 };
 
 /* key definitions */
@@ -64,12 +65,17 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
+static const char *amixerup[] = {"amixer", "-c", "0", "set", "Master", "5+", "unmute", NULL};
+static const char *amixerdown[] = {"amixer", "-c", "0", "set", "Master", "5-", "unmute", NULL};
+static const char *amixermut[] = {"amixer", "sset", "Master", "Toggle", NULL};
+static const char *brightup[]	= { "xbacklight", "-inc", "10", NULL };
+static const char *brightdown[]	= { "xbacklight", "-dec", "10", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -86,14 +92,14 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_Down,   moveresize,     {.v = "0x 25y 0w 0h" } },
-	{ MODKEY,                       XK_Up,     moveresize,     {.v = "0x -25y 0w 0h" } },
-	{ MODKEY,                       XK_Right,  moveresize,     {.v = "25x 0y 0w 0h" } },
-	{ MODKEY,                       XK_Left,   moveresize,     {.v = "-25x 0y 0w 0h" } },
-	{ MODKEY|ShiftMask,             XK_Down,   moveresize,     {.v = "0x 0y 0w 25h" } },
-	{ MODKEY|ShiftMask,             XK_Up,     moveresize,     {.v = "0x 0y 0w -25h" } },
-	{ MODKEY|ShiftMask,             XK_Right,  moveresize,     {.v = "0x 0y 25w 0h" } },
-	{ MODKEY|ShiftMask,             XK_Left,   moveresize,     {.v = "0x 0y -25w 0h" } },
+	{ ControlMask,                  XK_Down,   moveresize,     {.v = "0x 25y 0w 0h" } },
+	{ ControlMask,                  XK_Up,     moveresize,     {.v = "0x -25y 0w 0h" } },
+	{ ControlMask,                  XK_Right,  moveresize,     {.v = "25x 0y 0w 0h" } },
+	{ ControlMask,                  XK_Left,   moveresize,     {.v = "-25x 0y 0w 0h" } },
+	{ ControlMask|ShiftMask,        XK_Down,   moveresize,     {.v = "0x 0y 0w 25h" } },
+	{ ControlMask|ShiftMask,        XK_Up,     moveresize,     {.v = "0x 0y 0w -25h" } },
+	{ ControlMask|ShiftMask,        XK_Right,  moveresize,     {.v = "0x 0y 25w 0h" } },
+	{ ControlMask|ShiftMask,        XK_Left,   moveresize,     {.v = "0x 0y -25w 0h" } },
 	{ MODKEY|ControlMask,           XK_Up,     moveresizeedge, {.v = "t"} },
 	{ MODKEY|ControlMask,           XK_Down,   moveresizeedge, {.v = "b"} },
 	{ MODKEY|ControlMask,           XK_Left,   moveresizeedge, {.v = "l"} },
@@ -117,7 +123,13 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0,                       	    0x1008ff13, spawn,         {.v = amixerup } },
+	{ 0,                       	    0x1008ff11, spawn,         {.v = amixerdown } },
+	{ 0,                       	    0x1008ff12, spawn,         {.v = amixermut } },
+	{ 0,				            0x1008ff02, spawn,  	   {.v = brightup } },
+	{ 0,				            0x1008ff03, spawn,	       {.v = brightdown } },
+	{ MODKEY,                       XK_q,       killclient,    {0} },
+    { MODKEY|ControlMask,           XK_q,       quit,          {0} },
 };
 
 /* button definitions */
